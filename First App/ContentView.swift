abb058f67d16b27b11568f866d31eb8c2243ff85
josh <cha23071@byui.edu>
2025-12-25 01:07:17 -0700

    First commit

---
+++ First App/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  First App
+//
+//  Created by Joshua Chapman on 12/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .purple, .red]
+//
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+
+    @State private var selected = "Bowling"
+    @State private var id = 1
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .mint)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size:144))
+                            .foregroundColor(.white)
+                    )
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+            Spacer()
+            Button("Try again!") {
+                withAnimation() {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+//            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Joshua Chapman on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
